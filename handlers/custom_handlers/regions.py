81dce9e5791531abb4dbb8f22d683bc4e72c85fc
Slon4ek <Slon4ek2017@yandex.ru>
2024-03-06 16:04:20 +0300

    Реализован обработчик для команды /regions

---
+++ handlers/custom_handlers/regions.py
@@ -0,0 +1,24 @@
+from telebot.types import Message
+from config_data.api_config import get_all_stations
+from loader import bot
+from utils.api.yandex.info_def import get_region_list
+from states.help import HelpState
+
+
+@bot.message_handler(commands=['regions'])
+def get_regions(message: Message) -> None:
+    bot.set_state(message.from_user.id, HelpState.regions, message.chat.id)
+    bot.send_message(message.from_user.id, 'Список регионов какой страны вы хотели бы посмотреть?')
+
+
+@bot.message_handler(state=HelpState.regions)
+def show_regions(message: Message) -> None:
+    stations = get_all_stations()
+    regions = get_region_list(stations, message.text)
+    if regions:
+        bot.send_message(message.from_user.id, '\n'.join(regions))
+        bot.delete_state(message.from_user.id)
+    else:
+        bot.send_message(message.from_user.id, 'Что-то пошло не так, напишите название страны '
+                                               'точно как в списке стран из команды /countries')
+
