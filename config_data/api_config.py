7ea02cbdae14dd8a94fab9c2cd82c74ab88d9d76
Slon4ek <Slon4ek2017@yandex.ru>
2024-03-16 13:12:57 +0300

    Добавлена функция get_schedule

--- config_data/api_config.py
+++ config_data/api_config.py
@@ -1,3 +1,5 @@
+import datetime
+import json
 from typing import Dict
 
 import requests
@@ -5,7 +7,8 @@ import requests
 from config_data.config import (API_YANDEX_URL,
                                 API_YANDEX_KEY,
                                 STATION_LIST_ENDPOINT,
-                                NEAREST_STATION_ENDPOINT)
+                                NEAREST_STATION_ENDPOINT,
+                                INSTATION_ENDPOINT)
 
 
 def api_request(endpoint: str, params: Dict | None = None) -> requests.Response:
@@ -20,7 +23,7 @@ def api_request(endpoint: str, params: Dict | None = None) -> requests.Response:
     )
 
 
-def get_all_stations() -> Dict:
+def get_all_data() -> Dict:
     response = api_request(endpoint=STATION_LIST_ENDPOINT)
     if response.status_code == requests.codes.ok:
         return response.json()
@@ -33,5 +36,15 @@ def nearest_stations(latitude: float, longitude: float, radius: int, transport_t
               'transport_types': transport_type
               }
     response = api_request(endpoint=NEAREST_STATION_ENDPOINT, params=params)
+    if response.status_code == requests.codes.ok:
+        return response.json()
+
+
+def get_schedule(station_code: str, date: datetime = datetime.date.today()):
+    params = {
+        'station': station_code,
+        'date': date
+    }
+    response = api_request(endpoint=INSTATION_ENDPOINT, params=params)
     if response.status_code == requests.codes.ok:
         return response.json()
